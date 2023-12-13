d49651ce630818ad983f014ffabe86826034d6ae
MJFracess <mjfracess@gmail.com>
2023-12-12 20:27:58 -0800

    Completed Assignment 012

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -1,2 +1,44 @@
+/**
+ * @author Trevor Hartman
+ * @author ChatGPT
+ * @author MJ Fracess
+ *
+ * @since Version 1.0
+ */
 public class SortingAlgorithms {
+    public static void main(String[] args) {
+        int[] array = {64, 25, 12, 22, 11};
+        selectionSort(array);
+
+        System.out.println("Sorted array:");
+        printArray(array);
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in unsorted part of the array
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Utility method to print an array
+    public static void printArray(int[] arr) {
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
 }
+
